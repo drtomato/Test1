623b33cff713a534d9334fc3f8e9ce4757bd0654
Thomas Revesz <thomas.revesz@xeq.se>
2018-11-02 21:51:06 +0100

    Modified to include local notifications

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Plugins, LocalNotification } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+   async triggerEvent() {
+      const notifications: LocalNotification[] = [];
+
+      notifications.push({
+          title: 'Task is over',
+          body: 'This is the content',
+          id: 0,
+      });
+
+      await Plugins.LocalNotifications.schedule({notifications: notifications});
+  }
 
 }
